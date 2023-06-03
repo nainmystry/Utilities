a41cbb0ab08092b69a71f64f4cd56a6d8d1a68fb
nainmystry <88156958+nainmystry@users.noreply.github.com>
2023-06-03 23:40:35 +0530

    Add files via upload

---
+++ Utilities.Shared/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Utilities.Shared
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
