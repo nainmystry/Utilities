7ee5ff1ca124ad26909a1a7db3db4259183d5092
nainmystry <88156958+nainmystry@users.noreply.github.com>
2023-06-03 23:39:58 +0530

    DataModels SOA

---
+++ Utilities.DataModels/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Utilities.DataModels
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
