b04d03259c3e4c52d95b0c5659452f1d750a2150
nainmystry <88156958+nainmystry@users.noreply.github.com>
2023-06-03 23:39:06 +0530

    DataAccess SOA

---
+++ Utilities.DataAccess/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Utilities.DataAccess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
